eccf19e30c0b3d977cb9d1f53ac503ff3d3bf0d0
PN <pnikolaykosev@gmail.com>
2020-04-24 16:54:59 +0200

    Added tests

---
+++ src/test/java/pn/nutrimeter/service/services/impl/MicroTargetServiceImplTest.java
@@ -0,0 +1,92 @@
+package pn.nutrimeter.service.services.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import pn.nutrimeter.data.models.MicroTarget;
+import pn.nutrimeter.data.repositories.MicroTargetRepository;
+import pn.nutrimeter.error.IdNotFoundException;
+import pn.nutrimeter.service.models.MicroTargetServiceModel;
+import pn.nutrimeter.service.services.api.MicroTargetService;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MicroTargetServiceImplTest {
+
+    private static final String ID = "id";
+
+    @MockBean
+    MicroTargetRepository repository;
+
+    @Autowired
+    MicroTargetService service;
+
+    MicroTargetServiceModel model;
+
+    @BeforeEach
+    void setUp() {
+        this.model = mock(MicroTargetServiceModel.class);
+    }
+
+    @Test
+    public void create_withValidModel_shouldReturnCorrect() {
+        this.service.create(this.model);
+
+        ArgumentCaptor<MicroTarget> captor = ArgumentCaptor.forClass(MicroTarget.class);
+        verify(this.repository).saveAndFlush(captor.capture());
+
+        MicroTarget microTarget = captor.getValue();
+        assertNotNull(microTarget);
+    }
+
+    @Test
+    public void create_withNullModel_shouldThrow() {
+        assertThrows(IllegalArgumentException.class, () -> this.service.create(null));
+    }
+
+    @Test
+    public void getByLifeStageGroupId_withValidId_shouldReturnCorrect() {
+        this.model.setLifeStageGroupId(ID);
+        MicroTarget microTarget = mock(MicroTarget.class);
+        microTarget.setLifeStageGroupId(ID);
+        microTarget.setId(ID);
+
+        when(this.repository.findByLifeStageGroupId(this.model.getLifeStageGroupId())).thenReturn(Optional.of(microTarget));
+
+        MicroTargetServiceModel actual = this.service.getByLifeStageGroupId(this.model);
+
+        verify(this.repository).findByLifeStageGroupId(model.getLifeStageGroupId());
+        assertEquals(microTarget.getId(), actual.getId());
+        assertEquals(microTarget.getLifeStageGroupId(), actual.getLifeStageGroupId());
+    }
+
+    @Test
+    public void getByLifeStageGroupId_withNonExistingId_shouldThrow() {
+        assertThrows(IdNotFoundException.class, () -> this.service.getByLifeStageGroupId(this.model));
+    }
+
+    @Test
+    public void getByUserId_withExistingId_shouldReturnCorrect() {
+        MicroTarget microTarget = mock(MicroTarget.class);
+        microTarget.setId(ID);
+        microTarget.setLifeStageGroupId(ID);
+
+        when(this.repository.findByUserId(ID)).thenReturn(microTarget);
+
+        MicroTargetServiceModel actual = this.service.getByUserId(ID);
+
+        verify(this.repository).findByUserId(ID);
+        assertEquals(microTarget.getId(), actual.getId());
+        assertEquals(microTarget.getLifeStageGroupId(), actual.getLifeStageGroupId());
+    }
+}
